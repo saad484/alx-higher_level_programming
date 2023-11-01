aef3c8b1b59fc45a0c4962c010e0e66d0268c2b8
saad484 <aiddisaad484@gmail.com>
2023-11-01 23:33:10 +0100

    technical interview prepa

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -20,14 +20,4 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 listint_t *insert_node(listint_t **head, int number);
-
-/*Prototypes*/
-def islower(c):
-def uppercase(str):
-def print_last_digit(number):
-def add(a, b):
-def pow(a, b):
-def pow(a, b):
-
 #endif /* LISTS_H*/
-
