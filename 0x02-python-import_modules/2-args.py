cec16d442c74a442baea0613dd4fb011421c7623
saad484 <aiddisaad484@gmail.com>
2023-11-03 13:43:53 +0100

    print the arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) - 1 == 0:
+        print("0 arguments.")
+    else:
+        print("{} argument:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
