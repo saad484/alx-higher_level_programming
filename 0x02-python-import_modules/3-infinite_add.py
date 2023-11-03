06f80b3ee4ae794f26f4043db03cff732fde03ca
saad484 <aiddisaad484@gmail.com>
2023-11-03 21:47:11 +0100

    return the sum of the arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,9 +2,6 @@
 if __name__ == "__main__":
     import sys
     counter = 0
-    if len(sys.argv) == 1:
-        print(counter)
-    else:
-        for i in range(1, len(sys.argv)):
-            counter += int(sys.argv[i])
+    for i in range(1, len(sys.argv)):
+        counter += int(sys.argv[i])
 print("{}".format(counter))
