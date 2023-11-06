ef509bf05227106c7a96103fce28a7620f612cdc
saad484 <aiddisaad484@gmail.com>
2023-11-06 15:20:47 +0100

    replace elem

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    for i in my_list:
+        if idx < 0:
+            return my_list
+        elif idx > len(my_list)-1:
+            return my_list
+        else:
+            my_list[idx] = element
+            return my_list
