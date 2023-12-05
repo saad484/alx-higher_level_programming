591e6bf7ae0ec40ca88dc4374e317e41435a0c57
saad484 <aiddisaad484@gmail.com>
2023-12-06 00:48:43 +0100

    reading from a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """print the content of a UTF8 text file to stdout"""
     with open('my_file_0.txt', 'r', encoding='utf-8') as f:
-        print(f.read(), end="")
+        print(f.read())
