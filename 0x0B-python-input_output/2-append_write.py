d320fc933dab3a452c00381a80106a5af318b24c
saad484 <aiddisaad484@gmail.com>
2023-12-06 16:14:20 +0100

    append to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,8 +4,7 @@
 
 
 def append_write(filename="", text=""):
-    with open(filename, "a", encoding='utf-8') as f:
-        """Appends a string to the end of a UTF8 text file.
+    """Appends a string to the end of a UTF8 text file.
 
     Args:
         filename (str): The name of the file to append to.
@@ -13,4 +12,5 @@ def append_write(filename="", text=""):
     Returns:
         The number of characters appended.
     """
+    with open(filename, "a", encoding='utf-8') as f:
         return f.write(text)
