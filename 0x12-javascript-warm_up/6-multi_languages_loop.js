d47c66cb89e3f33cf4889f174668e91c218fdd70
saad484 <aiddisaad484@gmail.com>
2024-02-13 19:55:17 +0100

    foreach to print all the array elm

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const arr = ['C is fun','Python is cool', 'JavaScript is amazing'];
-arr.forEach((elm)=>{console.log(`${elm}`)});
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+arr.forEach((elm) => { console.log(`${elm}`); });
