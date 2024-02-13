b3ecb32483025349f269dbf3ef5371d03f181fde
saad484 <aiddisaad484@gmail.com>
2024-02-13 19:27:20 +0100

    print passed args

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+let argValue1 = process.argv[2] !== undefined ? process.argv[2] : "undefined";
+let argValue2 = process.argv[3] !== undefined ? process.argv[3] : "undefined";
+
+console.log(argValue1 + " is " + argValue2);
