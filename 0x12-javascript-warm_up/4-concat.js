130f1f47e9ef7964bfaba468119b6895108f33ef
saad484 <aiddisaad484@gmail.com>
2024-02-13 19:30:56 +0100

    prints passed param

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,2 @@
 #!/usr/bin/node
-let argValue1 = process.argv[2] !== undefined ? process.argv[2] : "undefined";
-let argValue2 = process.argv[3] !== undefined ? process.argv[3] : "undefined";
-console.log(argValue1 + " is " + argValue2);
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
