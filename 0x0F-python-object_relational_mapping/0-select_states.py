98f62a6c3e7cbac066f1bc8f5f4862ffbd8262d1
saad484 <aiddisaad484@gmail.com>
2024-02-18 22:07:51 +0100

    selct all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+  """Lists all the states from the databases hbtn_0e_0_usa"""
 import sys
 import MySQLdb
 
