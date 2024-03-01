faa1f4ba8bd21d6d1a281be53587be60816da746
saad484 <aiddisaad484@gmail.com>
2024-03-01 17:24:24 +0100

    return the size of body content

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/bash
 #Display the size of the body of the response
-if [ $# -eq 0 ]; then
-    echo "Please provide a URL as an argument."
-    exit 1
-fi
-response=$(curl -sI "$1" | grep -i Content-Length | awk '{print $2}' | tr -d '\r')
-echo "$response"
+curl -sI '$1' | grep 'Content-Length' | awk '{print $2}'
