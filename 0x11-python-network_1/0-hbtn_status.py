cb6df2c72dea8a606f0f92b1c28f0684df924a98
saad484 <aiddisaad484@gmail.com>
2024-03-04 16:57:42 +0100

    get body status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''This script fetches data from 
+    https://intranet.alxswe.com/status
+'''
+
+import urllib.request
+
+with urllib.request.urlopen("https://intranet.alxswe.com/status") as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
+
