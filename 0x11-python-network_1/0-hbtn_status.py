57889bd752a0f2d15099764b0380f49eb2f90533
saad484 <aiddisaad484@gmail.com>
2024-03-04 23:21:04 +0100

    fetches body response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 '''This script fetches data from
 https://alx-intranet.htbn.io/status'''
-import urllib.request
 
-with urllib.request.urlopen("https://alx-intranet.htbn.io/status") as response:
-    body = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode('utf-8')))
+if __main__ == "__main__":
+    import urllib.request
+    with urllib.request.urlopen("https://alx-intranet.htbn.io/status")\
+        as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
